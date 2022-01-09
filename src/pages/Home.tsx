b7a1378bcd2366be209f05ac179bccb97362a1e6
Germain Grumel <germaingrumel@outlook.fr>
2022-01-09 19:36:30 +0100

    added visuals on home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,21 @@
 import {
   IonButton,
+  IonButtons,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
   IonCol,
   IonContent,
   IonHeader,
+  IonImg,
+  IonItem,
+  IonList,
   IonLoading,
+  IonMenu,
   IonPage,
+  IonRouterOutlet,
   IonRow,
+  IonSplitPane,
   IonText,
   IonTitle,
   IonToolbar,
@@ -28,11 +38,24 @@ const Home: React.FC = () => {
   }
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Walk my Doggy</IonTitle>
-        </IonToolbar>
-      </IonHeader>
+      <IonMenu side="end" type="push" contentId="main">
+        <IonHeader>
+          <IonToolbar color="danger">
+            <IonTitle>End Menu</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent>
+          <IonList>
+            <IonItem>Menu Item</IonItem>
+            <IonItem>Menu Item</IonItem>
+            <IonItem>Menu Item</IonItem>
+            <IonItem>Menu Item</IonItem>
+            <IonItem>Menu Item</IonItem>
+          </IonList>
+        </IonContent>
+      </IonMenu>
+      <IonRouterOutlet id="main">a</IonRouterOutlet>
+
       <IonContent className="ion-align-items-center ion-justify-content-center">
         <IonRow className="ion-align-items-center ion-justify-content-center">
           <IonCol
@@ -40,10 +63,49 @@ const Home: React.FC = () => {
             size="12"
           >
             <IonText>Hello {username}</IonText>
-            <IonLoading message="Logging out..." duration={0} isOpen={busy} />
-            <IonButton onClick={logout}> Logout</IonButton>
           </IonCol>
         </IonRow>
+
+        {/* CARD 1 */}
+        <IonRow className="ion-align-items-center ion-justify-content-center">
+          <IonCol
+            className="ion-align-items-center ion-justify-content-center"
+            size="6"
+          >
+            <IonCard>
+              <IonCardHeader color="primary">I have a dog</IonCardHeader>
+              <IonCardContent>
+                Doggy Walky is a service that allows you to register your dog so
+                people can walk it when you're too busy.
+              </IonCardContent>
+            </IonCard>
+          </IonCol>
+
+          {/* CARD 2 */}
+          <IonCol
+            className="ion-align-items-center ion-justify-content-center"
+            size="6"
+          >
+            <IonCard>
+              <IonCardHeader color="primary">I don't have a dog</IonCardHeader>
+              <IonCardContent>
+                Doggy Walky is a service that allows you to walk other people
+                dogs and earn money for it.
+              </IonCardContent>
+            </IonCard>
+          </IonCol>
+        </IonRow>
+
+        <IonRow className="ion-align-items-center ion-justify-content-center">
+          <IonCol
+            className="ion-align-items-center ion-justify-content-center"
+            size="6"
+          >
+            <IonImg src="/assets/img/registerDogs.png" />
+          </IonCol>
+        </IonRow>
+        <IonLoading message="Logging out..." duration={0} isOpen={busy} />
+        <IonButton onClick={logout}> Logout</IonButton>
       </IonContent>
     </IonPage>
   );
